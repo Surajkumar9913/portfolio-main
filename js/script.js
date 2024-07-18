9931b7df832a745ca743019b789962a00eea5d46
Surajkumar9913 <surajyuva200@gmail.com>
2024-07-18 11:32:55 +0530

    Animation Added on scroll

--- js/script.js
+++ js/script.js
@@ -1,31 +1,43 @@
+// Typing text 
 var typed = new Typed(".typing-text",{
     strings : ["Programmer", "Web Developer", "Web Designer", "Front-End Developer","Full Stack Developer"],
     typeSpeed : 60,
     backSpeed : 40,
     loop : true
 })
+// Typing text end
 
+// Toggle Navbar
 const mobileMenu = document.getElementById('mobile-menu');
 const nav = document.querySelector('.nav');
 
 mobileMenu.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
+// Toggle Navbar end
 
+
+// tilt on image 
 function tilt(event, element) {
-    // Get the center of the element
+
     const rect = element.getBoundingClientRect();
     const centerX = rect.left + rect.width / 2;
     const centerY = rect.top + rect.height / 2;
 
-    // Calculate the mouse position relative to the center of the element
     const mouseX = event.clientX - centerX;
     const mouseY = event.clientY - centerY;
 
-    // Calculate the rotation angles
-    const tiltX = (mouseY / centerY) * 20; // Adjust 5 for sensitivity
-    const tiltY = -(mouseX / centerX) * 20; // Adjust 5 for sensitivity
-
-    // Apply the tilt effect
+  
+    const tiltX = (mouseY / centerY) * 20;
+    const tiltY = -(mouseX / centerX) * 20; 
+  
     element.style.transform = `perspective(1000px) rotateX(${tiltX}deg) rotateY(${tiltY}deg)`;
 }
+// tilt end
+
+
+// Scroll Animation on element
+AOS.init({
+    offset: 200,
+    duration: 800,
+  });
